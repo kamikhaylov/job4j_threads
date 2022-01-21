53a48ddd0c5a6a23160f68b3189d6634fba75520
konstantin <kotmix@bk.ru>
2022-01-21 11:52:55 +0400

    1.1. Состояние нити. [#229175]

---
+++ src/main/java/concurrent/ThreadState.java
@@ -0,0 +1,22 @@
+package concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {}
+        );
+        Thread second = new Thread(
+                () -> {}
+        );
+        System.out.println(first.getName());
+        System.out.println(second.getName());
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED
+                || second.getState() != Thread.State.TERMINATED) {
+            System.out.println("Нить 1 " + first.getState());
+            System.out.println("Нить 2 " + second.getState());
+        }
+        System.out.println("Работа завершена");
+    }
+}
