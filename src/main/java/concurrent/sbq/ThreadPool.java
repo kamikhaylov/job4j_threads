6adbfcf3821a587dcc0be408cc2ce784b9dace24
konstantin <kotmix@bk.ru>
2022-02-13 16:12:28 +0400

    1. Реализовать ThreadPool [#1099]

---
+++ src/main/java/concurrent/sbq/ThreadPool.java
@@ -0,0 +1,36 @@
+package concurrent.sbq;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ThreadPool {
+    private final List<Thread> threads = new LinkedList<>();
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(5);
+    private final int size = Runtime.getRuntime().availableProcessors();
+
+    public ThreadPool() {
+        for (int i = 0; i < size; i++) {
+            Thread thread = new Thread(() -> {
+                while (!Thread.currentThread().isInterrupted()) {
+                    try {
+                        tasks.poll().run();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            threads.add(thread);
+            thread.start();
+        }
+    }
+
+    public void work(Runnable job) throws InterruptedException {
+        tasks.offer(job);
+    }
+
+    public void shutdown() {
+        for (Thread thread : threads) {
+            thread.interrupt();
+        }
+    }
+}
