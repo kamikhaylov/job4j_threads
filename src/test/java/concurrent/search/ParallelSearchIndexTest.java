cbcecb1e409e929ed8fb41a44ff2857062110427
konstantin <kotmix@bk.ru>
2022-02-20 19:39:24 +0400

    3. ForkJoinPool [#315067 #267139]

--- src/test/java/concurrent/search/ParallelSearchIndexTest.java
+++ src/test/java/concurrent/search/ParallelSearchIndexTest.java
@@ -25,7 +25,7 @@ public class ParallelSearchIndexTest {
     public void whenArray35thenIndex() {
         String[] array = {"5", "4", "3", "2", "1", "9", "8", "7", "6", "5",
                 "10", "20", "30", "40", "50", "60", "70", "80", "90", "89",
-                "99", "88", "77", "66", "55", "44", "33", "22", "11",
+                "99", "88", "77", "66", "55", "44", "33", "22", "11", "23",
                 "15", "45", "35", "25", "20"};
         Integer expected = ParallelSearchIndex.search(array, "22");
         assertThat(expected, is(27));
