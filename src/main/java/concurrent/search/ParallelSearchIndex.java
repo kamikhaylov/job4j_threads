4cf41e68e32d4c17a143ccf96a240811415f1fee
konstantin <kotmix@bk.ru>
2022-02-20 14:01:16 +0400

    3. ForkJoinPool [#315067 #267139]

--- src/main/java/concurrent/search/ParallelSearchIndex.java
+++ src/main/java/concurrent/search/ParallelSearchIndex.java
@@ -1,18 +1,17 @@
 package concurrent.search;
 
-import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
-public class ParallelSearchIndex extends RecursiveTask<Integer> {
+public class ParallelSearchIndex<T> extends RecursiveTask<Integer> {
     private static final int SIZE_TO_SIMPLE_SEARCH = 10;
-    private final List list;
+    private final T[] array;
     private final int from;
     private final int to;
-    private final Integer element;
+    private final T element;
 
-    public ParallelSearchIndex(List list, int from, int to, Integer element) {
-        this.list = list;
+    public ParallelSearchIndex(T[] array, int from, int to, T element) {
+        this.array = array;
         this.from = from;
         this.to = to;
         this.element = element;
@@ -21,25 +20,30 @@ public class ParallelSearchIndex extends RecursiveTask<Integer> {
     @Override
     protected Integer compute() {
         if (from - to <= SIZE_TO_SIMPLE_SEARCH) {
-            return simpleSearch(list, element);
+            return simpleSearch(array, element);
         }
         int mid = (from + to) / 2;
-        ParallelSearchIndex searchLeft = new ParallelSearchIndex(list, from, mid, element);
-        ParallelSearchIndex searchRight = new ParallelSearchIndex(list, mid + 1, to, element);
+        ParallelSearchIndex<T> searchLeft = new ParallelSearchIndex(array, from, mid, element);
+        ParallelSearchIndex<T> searchRight = new ParallelSearchIndex(array, mid + 1, to, element);
         searchLeft.fork();
         searchRight.fork();
         return Math.max(searchLeft.join(), searchRight.join());
     }
 
-    public static Integer search(List list, Integer element) {
-        if (list == null || element == null) {
+    public static <T> Integer search(T[] array, T element) {
+        if (array.length == 0 || element == null) {
             return -1;
         }
         ForkJoinPool forkJoinPool = new ForkJoinPool();
-        return forkJoinPool.invoke(new ParallelSearchIndex(list, 0, list.size() - 1, element));
+        return forkJoinPool.invoke(new ParallelSearchIndex<T>(array, 0, array.length - 1, element));
     }
 
-    private Integer simpleSearch(List list, Integer element) {
-        return list.indexOf(element);
+    private Integer simpleSearch(T[] array, T element) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(element)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
--- src/test/java/concurrent/search/ParallelSearchIndexTest.java
+++ src/test/java/concurrent/search/ParallelSearchIndexTest.java
@@ -2,24 +2,32 @@ package concurrent.search;
 
 import org.junit.Test;
 
-import java.util.List;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class ParallelSearchIndexTest {
 
     @Test
-    public void whenList9Toindex() {
-        List<Integer> list = List.of(5, 4, 3, 2, 1, 9, 8, 7, 6);
-        Integer expected = ParallelSearchIndex.search(list, 9);
+    public void whenArray9thenIndex() {
+        Integer[] array = {5, 4, 3, 2, 1, 9, 8, 7, 6};
+        Integer expected = ParallelSearchIndex.search(array, 9);
         assertThat(expected, is(5));
     }
 
     @Test
-    public void whenList12Toindex() {
-        List<Integer> list = List.of(5, 4, 3, 2, 1, 9, 8, 7, 6, 22, 33, 11);
-        Integer expected = ParallelSearchIndex.search(list, 22);
+    public void whenArray12thenIndex() {
+        Integer[] array = {5, 4, 3, 2, 1, 9, 8, 7, 6, 22, 33, 11};
+        Integer expected = ParallelSearchIndex.search(array, 22);
         assertThat(expected, is(9));
+    }
+
+    @Test
+    public void whenArray35thenIndex() {
+        String[] array = {"5", "4", "3", "2", "1", "9", "8", "7", "6", "5",
+                "10", "20", "30", "40", "50", "60", "70", "80", "90", "89",
+                "99", "88", "77", "66", "55", "44", "33", "22", "11",
+                "15", "45", "35", "25", "20"};
+        Integer expected = ParallelSearchIndex.search(array, "22");
+        assertThat(expected, is(27));
     }
 }
